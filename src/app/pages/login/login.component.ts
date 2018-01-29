ad0f6a93ff05629dd559691f5f6c8ee3e2876280
Patrick Ayres <briceayres@gmail.com>
2018-01-28 21:20:15 -0600

    COMPLETE recording Section 3.3 auth service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,28 +1,37 @@
-import { LoadingService } from './../../servies/loading.service';
-import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertType } from './../../enums/alert-type.enum';
 import { Alert } from './../../classes/alert';
 import { AlertService } from './../../services/alert.service';
+import { Subscription } from 'rxjs/Subscription';
+import { LoadingService } from './../../servies/loading.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   public loginForm: FormGroup;
+  private subscriptions: Subscription[] = [];
+  private returnUrl: string;
 
   constructor(
     private fb: FormBuilder, 
     private alertService: AlertService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.createForm();
    }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/chat';
   }
 
   private createForm(): void {
@@ -33,22 +42,29 @@ export class LoginComponent implements OnInit {
   }
 
   public submit(): void {
-    this.loadingService.isLoading.next(true);
-
+    
     if (this.loginForm.valid) {
-      // TODO call the auth service
+      this.loadingService.isLoading.next(true);
       const {email, password} = this.loginForm.value;
-      console.log(`Email: ${email}, Password: ${password}`);
-      this.loadingService.isLoading.next(false);
+
+      // TODO call the auth service
+      this.subscriptions.push(
+        this.auth.login(email, password).subscribe(success => {
+          if (success) {
+            this.router.navigateByUrl(this.returnUrl);
+          }
+          this.loadingService.isLoading.next(false);
+        })
+      );
     } else {
       const failedLoginAlert = new Alert('Your email or password were inavalid, try again.', AlertType.Danger);
-      setTimeout(() => {
-        this.loadingService.isLoading.next(false);
-        this.alertService.alerts.next(failedLoginAlert);
-      }, 2000);
-      
+      this.loadingService.isLoading.next(false);
+      this.alertService.alerts.next(failedLoginAlert);
     }
-   
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }

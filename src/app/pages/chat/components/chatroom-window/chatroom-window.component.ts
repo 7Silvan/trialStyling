832185fa9c9f31c8282c9e8f31d23e655f2edf01
Patrick Ayres <briceayres@gmail.com>
2018-02-12 01:01:47 -0600

    COMPLETE Section 5.5 - submitting new chat messages

--- src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
+++ src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { ChatroomService } from './../../../../services/chatroom.service';
@@ -10,7 +10,9 @@ import { LoadingService } from './../../../../servies/loading.service';
   templateUrl: './chatroom-window.component.html',
   styleUrls: ['./chatroom-window.component.scss']
 })
-export class ChatroomWindowComponent implements OnInit, OnDestroy {
+export class ChatroomWindowComponent implements OnInit, OnDestroy, AfterViewChecked {
+
+  @ViewChild('scrollContainer') private scrollContainer: ElementRef;
 
   private subscriptions: Subscription[] = [];
   public chatroom: Observable<any>;
@@ -24,19 +26,19 @@ export class ChatroomWindowComponent implements OnInit, OnDestroy {
     this.subscriptions.push(
       this.chatroomService.selectedChatroom.subscribe(chatroom => {
         this.chatroom = chatroom;
-        this.loadingService.isLoading.next(false);
       })
     );
 
     this.subscriptions.push(
       this.chatroomService.selectedChatroomMessages.subscribe(messages => {
         this.messages = messages;
-        this.loadingService.isLoading.next(false);
       })
     );
   }
 
   ngOnInit() {
+    this.scrollToBottom();
+
     this.subscriptions.push(
       this.route.paramMap.subscribe(params => {
         const chatroomId = params.get('chatroomId');
@@ -47,6 +49,16 @@ export class ChatroomWindowComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    try {
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    } catch(err) {}
   }
 
 }
--- src/app/services/chatroom.service.ts
+++ src/app/services/chatroom.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { LoadingService } from './../servies/loading.service';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class ChatroomService {
@@ -14,11 +15,11 @@ export class ChatroomService {
 
   constructor(
     private db: AngularFirestore,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private authService: AuthService
   ) {
     this.selectedChatroom = this.changeChatroom.switchMap(chatroomId => {
       if (chatroomId) {
-        this.loadingService.isLoading.next(true);
         return db.doc(`chatrooms/${chatroomId}`).valueChanges();
       }
       return Observable.of(null);
@@ -26,13 +27,27 @@ export class ChatroomService {
 
     this.selectedChatroomMessages = this.changeChatroom.switchMap(chatroomId => {
       if (chatroomId) {
-        this.loadingService.isLoading.next(true);
-        return db.collection(`chatrooms/${chatroomId}/messages`).valueChanges();
+        return db.collection(`chatrooms/${chatroomId}/messages`, ref => {
+          return ref.orderBy('createdAt', 'desc').limit(100);
+        })
+        .valueChanges()
+        .map(arr => arr.reverse());
       }
       return Observable.of(null);
     });
 
     this.chatrooms = db.collection('chatrooms').valueChanges();
-   }
+  }
+
+  public createMessage(text: string): void {
+    const chatroomId = this.changeChatroom.value;
+    const message = {
+      message: text,
+      createdAt: new Date(),
+      sender: this.authService.currentUserSnapshot
+    };
+
+    this.db.collection(`chatrooms/${chatroomId}/messages`).add(message);
+  }
 
 }
